b9dbf910230b440f7a64f8c980054a939f7c4759
Chalana Kalpitha <chalana@bistecglobal.com>
2024-07-01 13:34:49 +0530

    routing set

--- client/finance-tracker/src/App.tsx
+++ client/finance-tracker/src/App.tsx
@@ -1,12 +1,17 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<h1>Dashboard</h1>} />
+          <Route path="/auth" element={<h1>Sign In</h1>} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
